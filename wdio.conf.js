742bb614f315c98220648d24dc4c150efe0523ed
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-05-19 20:23:15 -0500

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -135,7 +135,7 @@ exports.config = {
     reporters: [
         'spec',
         ['junit', {
-            outputDir: './',
+            outputDir: './report',
             outputFileFormat: function(options) { // optional
                 return `results.xml`
             }
