26838d1832aed5d81dcb15e9f9dbb0ed65deea80
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-05-19 20:19:18 -0500

    Updates (#6) (#7)
    * Add files via upload
    
    * Create .dockerignore

--- wdio.conf.js
+++ wdio.conf.js
@@ -132,7 +132,15 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './',
+            outputFileFormat: function(options) { // optional
+                return `results.xml`
+            }
+        }]
+    ],
 
 
     
